128f8e946f30dee88948499c27b007f596c68fe8
Roland Stigge <stigge@antcom.de>
2012-12-05 14:44:45 +0100

    0.63.2-1.1 (patches unapplied)
    Imported using git-ubuntu import.

--- lib/Sbuild/Build.pm
+++ lib/Sbuild/Build.pm
@@ -177,16 +177,14 @@ sub set_version {
     my ($pkg, $version) = split /_/, $pkgv;
     my $pver = Dpkg::Version->new($version, check => 1);
     return if (!defined($pkg) || !defined($version) || !defined($pver));
-    my ($o_version, $o_revision);
+    my ($o_version);
     $o_version = $pver->version();
-    $o_revision = $pver->revision();
-    $o_revision = "" if $pver->{'no_revision'};
 
     # Original version (no binNMU or other addition)
     my $oversion = $version;
     # Original version with stripped epoch
     my $osversion = $o_version;
-    $osversion .= '-' . $o_revision if $o_revision;
+    $osversion .= '-' . $pver->revision() unless $pver->{'no_revision'};
 
     # Add binNMU to version if needed.
     if ($self->get_conf('BIN_NMU') || $self->get_conf('APPEND_TO_VERSION')) {
